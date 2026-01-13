7b03c9e6d5437e33b570547bd8c6a4491d3cff97
Joshua Long <131176908+Sadliquid@users.noreply.github.com>
2026-01-13 15:00:16 +0800

    fixed checkHeaders (#3)

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException, status, Depends
 from dotenv import load_dotenv
+from functools import wraps
 import os
-import inspect
 
 load_dotenv()
 
@@ -17,12 +17,8 @@ async def _verify_api_key(api_key: str = Header(None, alias="API_KEY")):
     return api_key
 
 def checkHeaders(func):
-    if inspect.iscoroutinefunction(func):
-        async def wrapper(*args, _api_key: str = Depends(_verify_api_key), **kwargs):
-            return await func(*args, **kwargs)
-    else:
-        def wrapper(*args, _api_key: str = Depends(_verify_api_key), **kwargs):
-            return func(*args, **kwargs)
+    @wraps(func)
+    async def wrapper(*args, _api_key: str = Depends(_verify_api_key), **kwargs):
+        return await func(*args, **kwargs)
 
-    wrapper.__name__ = func.__name__
     return wrapper
